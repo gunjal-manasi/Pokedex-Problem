fedc3d9572bf8b3335a555d71f87145c85344ee1
Manasi Gunjal <gunjalmanasi6@gmail.com>
2026-03-26 18:03:37 +0530

    force dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { Container, Typography, Box } from "@mui/material";
 import { FilterablePokedexTable } from "@/components/FilterablePokedexTable";
